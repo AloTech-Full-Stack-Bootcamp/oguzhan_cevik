f6348aaa94fbbc7c26ead258b7d1f2d07dc1c1f6
Oğuzhan  Çevik <43252958+worm-codes@users.noreply.github.com>
2021-10-29 20:34:55 +0300

    Update app.js

--- week-1/express_serverr/app.js
+++ week-1/express_serverr/app.js
@@ -1,14 +1,19 @@
 var express=require("express"),
 app=express()
 
-app.set("view engine","ejs");
+//if you are not familiar with ejs(embedded-javascript-templates) :
+// EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.
 
+app.set("view engine","ejs"); // installed ejs package to do not write .ejs it will automaticly recognize the view engine as ejs
+//redirecting de index page and rendering index ejs file
 app.get("/",(req,res)=>{
 	res.render("index");
 });
+//redirecting de about page and rendering about ejs file
 app.get("/about",(req,res)=>{
 	res.render("about");
 });
+//redirecting de contact page and rendering contact ejs file
 app.get("/contact",(req,res)=>{
 	res.render("contact");
 });
